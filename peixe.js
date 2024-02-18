6690e8f69c8a285252ffa0437a9e05c33b457640
Cibele <158760135+cl34-inteli@users.noreply.github.com>
2024-02-18 14:51:03 -0300

    Atividade Tartaruguinha

--- peixe.js
+++ peixe.js
@@ -1,5 +1,7 @@
 var config = {
     type: Phaser.AUTO,
+
+    //define o espaço ocupado na tela, nesse caso, 800 de largura e 600 de altura
     width: 800,
     height: 600,
 
@@ -12,8 +14,10 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+//cria uma variável chamada peixinho
 var peixinho
 
+//carrega as coisas dentro da página
 function preload() {
     this.load.image('mar', 'assets/bg_azul-claro.png');
     this.load.image('logo', 'assets/logo-inteli_azul.png');
@@ -22,15 +26,19 @@ function preload() {
     this.load.image('crustaceo', 'assets/peixes/crustaceo.png');
 }
 
+//coloca as imagens dentro da página
 function create() {
     this.add.image(400, 300, 'mar')
     this.add.image(400,525, 'logo').setScale(0.5);
     this.add.image(100,225, 'concha').setScale(1.5);
     this.add.image(700,125, 'crustaceo').setScale(0.5);
+
+    //É uma variável controlável
     peixinho = this.add.image(400,300, 'peixe');
     peixinho.setFlip(true, false);
 }
 
+//trabalha como o while atualizando as informções dentro da página, nesse caso faz a tartaruguinha seguir a seta do mouse
 function update() {
     peixinho.x = this.input.x;
     peixinho.y = this.input.y;
